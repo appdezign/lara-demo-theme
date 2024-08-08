3952da49f7b1957faf0abce70bc5436f528dd1a8
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2024-08-08 15:04:58 +0200

    Add Cookie JS

--- views/_main/html_footer.blade.php
+++ views/_main/html_footer.blade.php
@@ -3,3 +3,20 @@
 @else
 	{!! Theme::js('js/app.js?ver='.date("YmdHis")) !!}
 @endif
+
+{!! Theme::js('vendor/jscookie/js.cookie.js') !!}
+
+<script>
+	let dropdownParents = document.querySelectorAll('[data-bs-toggle="dropdown"]');
+	dropdownParents.forEach(el => {
+		el.addEventListener('click', (e) => {
+			let width = document.body.clientWidth;
+			if(width > 992) {
+				let dropdownDirectUrl = e.target.href;
+				if(dropdownDirectUrl && dropdownDirectUrl != '#') {
+					location.href = dropdownDirectUrl;
+				}
+			}
+		});
+	});
+</script>
