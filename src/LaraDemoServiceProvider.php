2de6bdf440a8ca3b03fcfd2074e345b473e60d7c
Sybrand Hoeksma <s.hoeksma@firmaq.nl>
2024-11-14 13:26:33 +0100

    Add media to assets

--- src/LaraDemoServiceProvider.php
+++ src/LaraDemoServiceProvider.php
@@ -20,6 +20,7 @@ class LaraDemoServiceProvider extends ServiceProvider
 			__DIR__.'/../_assets/_public/fonts' => public_path('assets/themes/demo/fonts'),
 			__DIR__.'/../_assets/_public/images' => public_path('assets/themes/demo/images'),
 			__DIR__.'/../_assets/_public/vendor' => public_path('assets/themes/demo/js/vendor'),
+			__DIR__.'/../_assets/_public/media' => public_path('assets/media'),
 		], 'lara');
 
 	}
